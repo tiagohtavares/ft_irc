159457dd4bd20bb1e223889e1c127202ba874b34
tiagohtavares <tiagotavares262@gmail.com>
2024-09-19 15:24:08 +0100

    Fixes

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -12,6 +12,18 @@
 
 #include "../includes/Server.hpp"
 
+bool isNumerical(const char* str)
+{
+	for (size_t i = 0; i < strlen(str); i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 3)
@@ -19,10 +31,29 @@ int main(int argc, char **argv)
 		std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
 		return -1;
 	}
+
+	if (!isNumerical(argv[1]))
+	{
+		std::cerr << "Error: Port number must be numeric." << std::endl;
+		return -1;
+	}
+
 	//Check ports range and numerical.
 	int port = atoi(argv[1]);
+
+	if (port < 1 || port > 65535)
+	{
+		std::cerr << "Error: Port number must be between 1 and 65535." << std::endl;
+		return -1;
+	}
+
 	std::string password = argv[2];
 
+	if (password.length() > 20)
+	{
+		std::cerr << "Error: Password is too long. Maximum length is 20 characters." << std::endl;
+		return -1;
+	}
 	Server server(port, password);
 	server.run();
 
